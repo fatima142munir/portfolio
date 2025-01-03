6a0ca27f89b11b945f7a7cf5ccef56c99696b36f
Fatima Munir <developerfatima96@gmail.com>
2025-01-02 20:07:16 -0800

    update contact section

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,7 +21,7 @@ const Contact = () => {
                 </div>
                 <div className="lg:w-1/3 md:w-1/2 bg-gray-200 p-8 shadow-white shadow-2xl rounded-lg flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-                    <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+                    <p className="leading-relaxed mb-5 text-gray-600">Happy to connect!</p>
                     <div className="relative mb-4">
                         <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
                         <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
@@ -35,7 +35,6 @@ const Contact = () => {
                         <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                     </div>
                     <button className="text-white bg-darkest border-0 py-2 px-6 focus:outline-none hover:bg-dark rounded text-lg">Button</button>
-                    <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
                 </div>
             </div>
         </section>
